e926ff4ab2ac7beb6bbbab4c200e2d71cc08d192
Martin Cabrera <martin@MacBook-Pro-de-Martin.local>
2026-02-10 00:28:55 -0300

    🎨 Fix Tailwind color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                border: "hsl(var(--border))",
+                background: "hsl(var(--background))",
+                foreground: "hsl(var(--foreground))",
+                card: "hsl(var(--card))",
+                primary: "hsl(var(--primary))",
+                secondary: "hsl(var(--secondary))",
+                muted: "hsl(var(--muted))",
+                "muted-foreground": "hsl(var(--muted-foreground))",
+            },
+        },
     },
     plugins: [],
 }
